e70b74a0fd9827770554f4811377a5c03c8a0c88
Jorge <jorge.toledo.6@hotmail.com>
2022-03-24 14:12:51 -0300

    Clase 24 Marzo

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresa un Numero (MAX 3 Cifras): ");
+            var num = Convert.ToInt32(Console.ReadLine());
+
+            var resultado = num / 100;
+
+            Console.WriteLine($"Cantidad de Centenas: " + resultado.ToString());
+            Console.WriteLine($"Cantidad de Centenas: { num / 100 }");
+            Console.WriteLine("Cantidad de Centenas: {0} {1}", 
+                resultado.ToString(), resultado.ToString());
+            
+            
+            Console.WriteLine($"Cantidad de Decenas: { (num % 100) / 10 }");
+            Console.WriteLine($"Cantidad de Unidades: { (num % 10) }");
+        }
+    }
+}
---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> listaEjemplo = new List<string>();
+
+            //Metodo ADD
+            listaEjemplo.Add("String");
+
+            //Recorrer Lista
+            foreach (var item in listaEjemplo)
+            {
+                Console.WriteLine(item);
+            }
+            listaEjemplo.ForEach(cliente => Console.WriteLine(cliente.ToLower()));
+
+            //Acceder a Posicion Especifica (INDEX)
+            var primerItem = listaEjemplo[0];
+
+            //Listado vacio
+            List<Cliente> listaClientes = new List<Cliente>();
+
+            Cliente juanitoPerez = new Cliente("Juan", "Perez", "a@a.cl");
+
+            listaClientes.Add(juanitoPerez);
+
+            listaClientes.ForEach(cliente => Console.WriteLine(cliente.Nombre));
+
+            Console.WriteLine(juanitoPerez.ToString());
+        }
+
+    }
+
+    public class Cliente
+    {
+        public string Nombre;
+        public string Apellido;
+        public string Correo;
+
+        public Cliente(string nombre, string apellido, string correo)
+        {
+            this.Nombre = nombre;
+            this.Apellido = apellido;
+            this.Correo = correo;   
+        }
+
+        public override string ToString()
+        {
+            return $"Nombre: {Nombre} Apellido: {Apellido} Correo: {Correo}";
+        }
+    }
+}
