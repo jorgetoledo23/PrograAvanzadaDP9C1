9a2e9888ecdb435df81b1bc9bad321fce0b45466
Jorge <jorge.toledo.6@hotmail.com>
2022-04-28 14:00:05 -0400

    28 Abril

---
+++ APIEmpleados/Model/AppDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace APIEmpleados.Model
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var connection = configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connection);
+        }
+
+        //API FLUENT
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //modelBuilder.Entity<Empleado>().HasKey(prop => prop.Rut);
+            modelBuilder.Entity<Empleado>().Property(prop => prop.FechaIngreso)
+                .HasColumnType("date");
+
+            modelBuilder.Entity<Empleado>().Property(prop => prop.Activo)
+                .HasDefaultValue(true);
+
+            //Filtro a nivel del Modelo
+            modelBuilder.Entity<Empleado>()
+                .HasQueryFilter(prop => prop.Activo == true);
+
+        }
+
+
+        public DbSet<Empleado> Empleados { get; set; }
+
+        public DbSet<Departamento> Departamentos { get; set; }
+
+    }
+}
---
+++ APIEmpleados/Model/Empleado.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace APIEmpleados.Model
+{
+    public class Empleado
+    {
+        //[Key]
+        public int EmpleadoId { get; set; }
+
+        [Required]
+        [StringLength(12)]
+        public string Rut { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+        
+        [Required]
+        public string Apellidos { get; set; }
+        
+        [Required]
+        public string CodigoEmpleado { get; set; }
+
+        public string Telefono { get; set; }
+
+        public string Correo { get; set; }
+
+        public DateTime FechaIngreso { get; set; }
+
+        public TipoEmpleado TipoEmpleado { get; set; }
+
+        public bool Activo { get; set; }
+
+        public int DepartamentoId { get; set; }
+        
+        //Prop de Navegacion
+        public Departamento Departamento { get; set; }
+
+    }
+}
