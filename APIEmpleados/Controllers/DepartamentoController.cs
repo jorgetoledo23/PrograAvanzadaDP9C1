bf9d33af0f860600e6091ed0da2c3a7266b4d3fa
Jorge <jorge.toledo.6@hotmail.com>
2022-05-05 13:19:43 -0400

    5 Mayo API

---
+++ APIEmpleados/Controllers/DepartamentoController.cs
@@ -0,0 +1,91 @@
+﻿using APIEmpleados.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIEmpleados.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartamentoController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public DepartamentoController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("GetAllDepartments")]
+        public async Task<ActionResult<List<Departamento>>> GetAllDepartments()
+        {
+            var Departments = _context.Departamentos.ToList();
+            return Ok(Departments);
+        }
+
+        [HttpGet]
+        [Route("GetDeparmentById")]
+        public async Task<ActionResult<Departamento>> GetDepartmentById(int id)
+        {
+            var Department = _context.Departamentos
+                .FirstOrDefault(dept => dept.DepartamentoId == id);
+            return Ok(Department);
+        }
+
+        [HttpPost]
+        [Route("AddDepartment")]
+        public async Task<ActionResult> AddDeparment(DepartamentoDTO deptDTO)
+        {
+            if (deptDTO == null) return BadRequest();
+            var departamento = new Departamento()
+            {
+                CompanyNo = deptDTO.CompanyNo,
+                Descripcion = deptDTO.Descripcion
+            };
+            _context.Departamentos.Add(departamento);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("UptDepartment")]
+        public async Task<ActionResult> UptDeparment(DepartamentoDTO deptDTO)
+        {
+            if (deptDTO == null) return BadRequest();
+            var departamento = new Departamento()
+            {
+                DepartamentoId = deptDTO.Id,
+                CompanyNo = deptDTO.CompanyNo,
+                Descripcion = deptDTO.Descripcion
+            };
+            _context.Departamentos.Update(departamento);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("DelDepartment")]
+        public async Task<ActionResult> DelDeparment(DepartamentoDTO deptDTO)
+        {
+            if (deptDTO == null) return BadRequest();
+            var dpto = _context.Departamentos
+                .FirstOrDefault(d => d.DepartamentoId == deptDTO.Id);
+            _context.Departamentos.Remove(dpto);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ APIEmpleados/Model/DepartamentoDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace APIEmpleados.Model
+{
+    public class DepartamentoDTO
+    {
+        public int Id { get; set; }
+        public string CompanyNo { get; set; }
+        public string Descripcion { get; set; }
+
+    }
+}
